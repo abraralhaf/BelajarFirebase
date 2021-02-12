926c6ae35184c8e46bd84ec37a319fd751141f9d
Alhuft <abrarsteam@gmail.com>
2021-02-12 15:15:22 +0700

    Belajar1

---
+++ app/src/main/java/com/alhuft/belajarfirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alhuft.belajarfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
