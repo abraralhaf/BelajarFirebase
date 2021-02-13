6eb3f0ed0fa1682abd294f214022fb242289fc14
Alhuft <abrarsteam@gmail.com>
2021-02-13 19:17:42 +0700

    saveway

--- app/src/main/java/com/alhuft/belajarfirebase/MainActivity.java
+++ app/src/main/java/com/alhuft/belajarfirebase/MainActivity.java
@@ -2,13 +2,31 @@ package com.alhuft.belajarfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this,"Logout",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), StartActivity.class));
+            }
+        });
     }
 }
