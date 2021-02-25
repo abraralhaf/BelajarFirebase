cf809c3667dcb2f7617dcfc9884f2828fa29a49a
Alhuft <abrarsteam@gmail.com>
2021-02-25 10:36:56 +0700

    25 February 2021

--- app/src/main/java/com/alhuft/belajarfirebase/MainActivity.java
+++ app/src/main/java/com/alhuft/belajarfirebase/MainActivity.java
@@ -1,19 +1,27 @@
 package com.alhuft.belajarfirebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,8 +31,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,9 +46,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button logout, add;
+    private Button logout, upload, add;
     private EditText edit;
     private ListView listView;
+    private Uri imgUri;
+    private static final int IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +61,21 @@ public class MainActivity extends AppCompatActivity {
         edit = findViewById(R.id.edit);
         add = findViewById(R.id.add);
         listView = findViewById(R.id.listView);
+        upload = findViewById(R.id.upload);
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this,"Logout",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(), StartActivity.class));
+            }
+        });
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImage();
+                // uploadImage();
             }
         });
         ////FirebaseDatabase.getInstance().getReference().child("Belajar").child("Android").setValue("abcd");
@@ -66,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
                 String txt_name = edit.getText().toString();
                 if (txt_name.isEmpty()) {
                     Toast.makeText(MainActivity.this, "No name entered!", Toast.LENGTH_SHORT).show();
-                }else {
-                   // FirebaseDatabase.getInstance().getReference().child("Languages").push().child("n5").setValue(txt_name);
+                } else {
+                    // FirebaseDatabase.getInstance().getReference().child("Languages").push().child("n5").setValue(txt_name);
                     FirebaseDatabase.getInstance().getReference().child("Languages").child("n5").setValue(txt_name);
                     Toast.makeText(MainActivity.this, "Added Success!", Toast.LENGTH_SHORT).show();
                 }
@@ -86,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String ammount = snapshot.child("email").getValue(String.class);
                     String brand = snapshot.child("name").getValue(String.class);
-                    Log.d("TAG",ammount + " + " + brand);
+                    Log.d("TAG", ammount + " + " + brand);
                     Information info = snapshot.getValue(Information.class);
                     String text = info.getName() + " : " + info.getEmail();
                     list.add(text);
@@ -101,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         Map<String, Object> data = new HashMap<>();
-        data.put("capital", false);
+        data.put("capital", true);
         db.collection("cities").document("JSR").set(data, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -111,15 +136,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Map<String, Object> data2 = new HashMap<>();
-        data2.put("name", "Tokyo");
-        data2.put("capital", "Japan");
-        db.collection("cities").add(data2).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                Toast.makeText(MainActivity.this, "plos", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        Map<String, Object> data2 = new HashMap<>();
+//        data2.put("name", "Tokyo");
+//        data2.put("capital", "Japan");
+//        db.collection("cities").add(data2).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentReference> task) {
+//                Toast.makeText(MainActivity.this, "plos", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 //        Map<String, Object> city = new HashMap<>();
 //        city.put("name", "A");
 //        city.put("state", "Jharkhan");
@@ -133,7 +158,79 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
+//        DocumentReference docRef = FirebaseFirestore.getInstance().collection("cities").document("BJ");
+//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot doc = task.getResult();
+//                    if (doc.exists()) {
+//                        Log.d("Document", doc.getData().toString());
+//                    }else{
+//                        Log.d("Document", "no data");
+//                    }
+//                }
+//            }
+//        });
+
+        FirebaseFirestore.getInstance().collection("cities").whereEqualTo("capital", true)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        Log.d("Document", doc.getId() + "=>" + doc.getData());
+                    }
+                }
+            }
+        });
     }
 
+    private void openImage() {
+        Intent i = new Intent();
+        i.setType("image/");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, IMAGE_REQUEST);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST && resultCode == RESULT_OK) {
+            imgUri = data.getData();
+            uploadImage();
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+    }
+
+    private void uploadImage() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("uploading");
+        pd.show();
+        if (imgUri != null) {
+            StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("uploads").child(System.currentTimeMillis() + "," + getFileExtension(imgUri));
+            fileRef.putFile(imgUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            Log.d("DownloadUrl", url);
+                            pd.dismiss();
+                            Toast.makeText(MainActivity.this, "Image Upload successfull", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+        }
+    }
 }
